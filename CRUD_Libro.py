67681722b4f019b29ed37d38c204b18b3da352ee
2025310075 <2025310075@uteq.edu.mx>
2026-07-19 14:51:54 -0600

    Agregar método Agregar_Libro y Seleccionar_Libro

--- CRUD_Libro.py
+++ CRUD_Libro.py
@@ -15,3 +15,52 @@ class CRUD_Libro:
     LiAutor.delete(0, tk.END)
     LiAño.delete(0, tk.END)
     
+ def agregar_Libro(self,LiNombre, LiAutor,LiAño,tabla):
+    nombre = LiNombre.get()
+    autor = LiAutor.get()
+    año = LiAño.get()
+
+
+    if nombre == "" or autor == "" or año=="":
+        messagebox.showwarning("Error", "No se pueden dejar campos vacíos")
+        return
+
+
+    # CREATE
+    agregar_libro = {
+        "id": self.ID_Lib,
+        "nombre": nombre,
+        "autor": autor,
+        "año": año,
+    }
+    self.libros.append(agregar_libro)
+    self.ID_Lib += 1
+   
+    self.actualizar_tabla(tabla)
+    self.limpiar(LiNombre, LiAutor,LiAño)
+    messagebox.showinfo("Correcto", "El libro se registrócorrectamente.")
+
+
+def seleccionar_Libro(self,event,tabla,LiNombre,LiAutor,LiAño):
+    # READ
+    try:
+        seleccion = tabla.selection()[0]
+        valores = tabla.item(seleccion, 'values')
+
+
+        self.limpiar(LiNombre, LiAutor,LiAño)
+        #Guardar el id
+
+
+        self.ID_sele = int(valores[0])
+
+
+        LiNombre.insert(0, valores[1])
+        LiAutor.insert(0, valores[2])
+        LiAño.insert(0, valores[3])
+    except IndexError:
+        pass
+
+
+
+    

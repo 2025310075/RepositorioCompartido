9fc8fc84118a0acf310682c241c383248436c85a
2025310209 <2025310209@uteq.edu.mx>
2026-07-19 18:47:29 -0600

    Se agrego metodo actualizar y eliminar

--- autores.py
+++ autores.py
@@ -7,4 +7,16 @@ class Autor:
         
     def mostrar_autor(self):
      return f"Nombre: {self.nombre}, Nacionalidad: {self.nacionalidad}, Edad: {self.edad}"
+   
+    def actualizar_autor(self, nuevo_nombre, nueva_nacionalidad, nueva_edad):
+     self.nombre = nuevo_nombre
+     self.nacionalidad = nueva_nacionalidad
+     self.edad = nueva_edad
+     return "Autor actualizado correctamente"
+
+    def eliminar_autor(self):
+     self.nombre = ""
+     self.nacionalidad = ""
+     self.edad = 0
+     return "Autor eliminado"
         
